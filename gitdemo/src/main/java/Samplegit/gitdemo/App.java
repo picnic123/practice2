00a2e3b7aed51c88770a618afd2038bfde42cbcf
sony <sony@sony-PC>
2017-10-24 21:59:34 +1100

    first commit to my project

---
+++ gitdemo/src/main/java/Samplegit/gitdemo/App.java
@@ -0,0 +1,13 @@
+package Samplegit.gitdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
